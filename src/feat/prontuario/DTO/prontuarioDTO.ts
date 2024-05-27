d90d614fc2f32829de4f1210d78766b7c7a17dab
douglasnobree <douglasclaudino543@gmail.com>
2024-05-27 19:01:13 -0300

    feat: Atualizar modelo de Prontuario e controlador de criação

--- src/feat/prontuario/DTO/prontuarioDTO.ts
+++ src/feat/prontuario/DTO/prontuarioDTO.ts
@@ -1,20 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class prontarioDTO{
-
+    @ApiProperty()
     nome: string;
+    @ApiProperty()
     cpfTutor: string;
+    @ApiProperty()
     data_nascimento: Date;
+    @ApiProperty()
     porteFisico: string;
+    @ApiProperty()
     sexo: string;
+    @ApiProperty()
     doencas: string;
+    @ApiProperty()
     vacinas: string;
+    @ApiProperty()
     tutor: string;
+    @ApiProperty()
     endereco: string;
+    @ApiProperty()
     telefone: string;
+    @ApiProperty()
     raca: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     data_atualizacao?: Date;
+    @ApiProperty()
     data_cadastro?: Date;
+    @ApiProperty()
     data_exclusao?: Date;
-
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API para gerenciamento de um petshop')
+    .setVersion('1.0')
+    .addTag('petshop')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT || 3535);
 }
 bootstrap();
